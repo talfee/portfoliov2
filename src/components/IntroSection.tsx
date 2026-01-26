cd86c33f41cb271fa84111145ec63155509a37ce
talfee <88052205+talfee@users.noreply.github.com>
2026-01-26 14:21:10 -0500

    lowercase

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -19,15 +19,65 @@ export function IntroSection() {
             letterSpacing: '-0.01em',
           }}
         >
-          Thanks for visiting my website! <br></br> 
-          Most of my work-related info is on my <a
+          thanks for visiting my website! <br></br> 
+          most of my work-related info is on my <a
             href="https://www.linkedin.com/in/talfe/"
             target="_blank"
             rel="noopener noreferrer"
             className="underline underline-offset-4 decoration-current text-white/80 hover:text-white transition-colors duration-300"  >
-            Linkedin
+            linkedin
           </a>, so here are some things about me that aren't:
         </p>
+        <ul
+          className="text-white/90 mb-6 list-disc list-inside"
+          style={{
+            fontSize: '0.95rem',
+            fontWeight: 300,
+            lineHeight: 1.5,
+            letterSpacing: '-0.01em',
+          }}
+        >
+          <li className="mb-2">
+            i pivoted hard from biotech research into computer science 3/4ths the way into my
+            degree (it's never too late!).
+          </li>
+          <li className="mb-2">
+            i run to relieve stress (
+            <a
+              href="https://www.strava.com/athletes/132741723"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline underline-offset-4 decoration-current text-white/80 hover:text-white transition-colors duration-300"
+            >
+              my strava
+            </a>
+            ).
+          </li>
+          <li className="mb-2">
+            i like to think my academic detours (presenting at academic conferences and publishing
+            new research) have instilled in me a love of building challenging projects and
+            collaborating effectively.
+          </li>
+          <li className="mb-2">
+            i love reading books, but i love discussing them with others
+            even more (
+            <a
+              href="https://www.goodreads.com/user/show/74831519-talfee"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline underline-offset-4 decoration-current text-white/80 hover:text-white transition-colors duration-300"
+            >
+              my goodreads
+            </a>
+            ).
+          </li>
+          <li className="mb-2">
+            i have a british shorthair cat.
+          </li>
+          <li className="mb-2">
+            i chose software over biology because i'd rather problem solve sitting down in an ergonomic chair than standing up at a lab bench. (jokes. partly).
+          </li>
+        </ul>
         <p
           className="text-white/90 mb-6"
           style={{
@@ -37,45 +87,7 @@ export function IntroSection() {
             letterSpacing: '-0.01em',
           }}
         >
-          I pivoted hard from biotech research into computer science 3/4ths the way into my
-          degree (it's never too late!).
-          I run to relieve stress (
-          <a
-            href="https://www.strava.com/athletes/132741723"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="underline underline-offset-4 decoration-current text-white/80 hover:text-white transition-colors duration-300"
-          >
-            my strava
-          </a>
-          ). 
-          I like to think my academic detours (presenting at academic conferences and publishing
-          new research) have instilled in me a love of building challenging projects and
-          collaborating effectively.  
-          I love reading books, but I love discussing them with others
-          even more (
-          <a
-            href="https://www.goodreads.com/user/show/74831519-talfee"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="underline underline-offset-4 decoration-current text-white/80 hover:text-white transition-colors duration-300"
-          >
-            my goodreads
-          </a>
-          ).  
-          I have a british shorthair cat. 
-          I chose software over biology because I'd rather problem solve sitting down in an ergonomic chair than standing up at a lab bench. (Jokes. Partly).
-        </p>
-        <p
-          className="text-white/90 mb-6"
-          style={{
-            fontSize: '0.95rem',
-            fontWeight: 300,
-            lineHeight: 1.5,
-            letterSpacing: '-0.01em',
-          }}
-        >
-          If we have anything in common, don't hesitate to reach out and connect!
+          if we have anything in common, don't hesitate to reach out and connect!
         </p>
       </motion.div>
     </section>
